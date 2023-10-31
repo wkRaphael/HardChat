9af5f8419251a064dfb59c4aceb46ac80e8b5d7e
wkRaphael <baconmcnuggetz@gmail.com>
2023-10-31 04:11:33 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# HardChat
-Easy Chat but bad
+EasyChat but worse(?)
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    let inputText;
+    //this works 
+    let Messages=[
+        {id: 0, who: 0, text: "my message"},
+        {id: 1, who: 1, text: "his message"}
+    ];
+    function sendMessage() {
+        Messages = Messages.concat({id: 2, who: 0, text: inputText.value})
+    }
+</script>
+<!-- https://svelte.dev/docs/introduction -->
+<body>
+    <button on:click={sendMessage}>Send</button>
+    <input bind:this={inputText}>
+    {#each Messages as myMessage}
+        {#if myMessage.who == 0}
+            <p class="myMessages">{myMessage.text}</p>
+        {:else if myMessage.who == 1}
+            <p class="othersMessages">{myMessage.text}</p>
+        {/if}
+    {/each}
+    
+</body>
+<style>
+    .myMessages{
+        color: green;
+    }
+    .othersMessages{
+        color: red;
+    }
+</style>
